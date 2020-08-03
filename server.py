4ff7ffce5906873f362b9311ffd17f96ebec5747
wengyeowyeap <aquamagmayeow94@gmail.com>
2020-08-03 12:20:44 +0800

    add models.py, update server.py

--- server.py
+++ server.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template, request
+from models import db # new line
 app = Flask(__name__)
+
+@app.before_request # new line
+def before_request():
+   db.connect()
+
+@app.after_request # new line
+def after_request(response):
+   db.close()
+   return response
 
 @app.route("/")
 def index():
